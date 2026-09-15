f3151df5267d4d4b6ad2eca05d9d1e537d022477
Amen Wakjira <amenwakjira@Amens-MacBook-Pro.local>
2026-09-15 16:47:23 -0500

    RPN done for more than one digit units in input

--- main.java
+++ main.java
@@ -1,5 +1,4 @@
 package calc; 
-import java.math.BigInteger;
 import java.util.*;  
 
 public class main {
--- update.java
+++ update.java
@@ -1,5 +1,4 @@
 package calc; 
-import java.math.*; 
 import java.util.*;  
 
 public class update {
@@ -16,39 +15,41 @@ public class update {
     
     // using Shunting Yard Algorithm to get RPN form of inputed string. 
     public static Queue<String> RPN_Form (String input) {
+        // for numbers and also output
         Queue<String> output = new LinkedList<>(); 
+        // for operations
         Stack<String> operations = new Stack<>(); 
         String noSpace = input.replaceAll("\\s+", ""); 
 
         for (int i = 0; i < noSpace.length(); i++) {
-            char token = noSpace.charAt(i);
-            if (Character.isDigit(token)) {
-                StringBuilder sb =  new StringBuilder(); 
-                sb.append(token); 
-                int skipto = i; 
-                while (skipto < noSpace.length() - 1 && Character.isDigit(noSpace.charAt(skipto + 1))) {
-                    sb.append(noSpace.charAt(skipto + 1)); 
-                    skipto++; 
+            int currentSpot = i; 
+            if (Character.isDigit(input.charAt(i))) {
+                String token = ""; 
+                while (i < input.length() && Character.isDigit(input.charAt(currentSpot))) {
+                    token = token + input.charAt(currentSpot); 
+                    currentSpot++;
+                    if (currentSpot == noSpace.length()) {
+                        break; 
+                    }
                 }
-                i = skipto;
-                System.out.println(sb); 
-                output.add(sb + ""); 
+                i = currentSpot - 1; 
+                output.add(token); 
             } else {
+                char oper = input.charAt(i); 
                 if (!operations.isEmpty()) {
-                    System.out.println(operations.peek()); 
                     int topValue = Precedence.get(operations.peek()); 
-                    while (topValue > Precedence.get(token + "") && !operations.isEmpty()) { 
+                    while (topValue > Precedence.get(oper + "") && !operations.isEmpty()) { 
                         topValue = Precedence.get(operations.peek()); 
                         output.add(operations.pop()); 
                     }
-                    operations.add(token + "");
+                    operations.add(oper + "");
                 } else {
-                    operations.add(token + ""); 
+                    operations.add(oper + ""); 
                 }
             }
         }
 
-        while (!operations.isEmpty()) {
+        while (!operations.isEmpty()) {  
             output.add(operations.pop()); 
         }
         return output; 
